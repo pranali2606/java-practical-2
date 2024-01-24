19b1caa585b476e271816af4e963e7d60a250b16
pranali2606 <155884358+pranali2606@users.noreply.github.com>
2024-01-24 10:20:26 +0530

    Create subtract.java
    created a subtract branch and added subtract function in it

---
+++ subtract.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+  public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 20;
+        int difference = subtract(a1, a2);
+    system.out.println("difference");
+  }
