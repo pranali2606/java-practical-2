8c1bfbe198ac92102a59b05844ba1b5fb20743e5
pranali2606 <155884358+pranali2606@users.noreply.github.com>
2024-01-29 09:35:43 +0000

    square  file in branch square

---
+++ square.java
@@ -0,0 +1,14 @@
+public class square {
+
+    public static int square(int a, int b) {
+        return a * b;
+    }
+
+public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 20;
+
+        int square =square(a1, a2);
+   System.out.println("square : " +square);
+}
+}
