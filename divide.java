8ca4c1a262658bdd6cfca495ab21f262457a76bb
pranali2606 <155884358+pranali2606@users.noreply.github.com>
2024-01-29 09:29:41 +0000

    divide file in branch divide

---
+++ divide.java
@@ -0,0 +1,14 @@
+public class divide {
+
+    public static int divide(int a, int b) {
+        return a * b;
+    }
+
+public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 20;
+
+        int divide = divide(a1, a2);
+   System.out.println("division : " + divide);
+}
+}
