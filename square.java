11cc011db0d5a15331f114ecc28300f84138f58c
pranali2606 <155884358+pranali2606@users.noreply.github.com>
2024-01-29 15:13:05 +0530

    Update square.java

--- square.java
+++ square.java
@@ -1,14 +1,13 @@
 public class square {
 
-    public static int square(int a, int b) {
-        return a * b;
+    public static int square(int a) {
+        return a * a;
     }
 
 public static void main(String[] args) {
         int a1 = 10;
-        int a2 = 20;
 
-        int square =square(a1, a2);
+        int square =square(a1);
    System.out.println("square : " +square);
 }
 }
