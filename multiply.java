c35319c8359a47e40adbbc0f876dd23f703f2902
pranali2606 <155884358+pranali2606@users.noreply.github.com>
2024-01-29 09:10:05 +0000

    adding multiplication function

---
+++ multiply.java
@@ -0,0 +1,14 @@
+public class multiply {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 20;
+
+        int multiply = multiply(a1, a2);
+   System.out.println("multiplication : " + multiply);
+}
+}
